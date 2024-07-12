f32216384ff31671cf71d90d89967838b37a9ebb
Abhishek Upadhyaya <abhishekupadhyaya477@gmail.com>
2024-07-12 21:54:25 +0530

    learnLater assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FlexLayoutModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent {
+  userRegistrationForm: any = '';
+  maxDate: Date = new Date();
+
+  constructor(private _snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.userRegistrationForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl(''),
+      dateOfBirth: new FormControl('', Validators.required),
+      age: new FormControl({ value: '', disabled: true }),
+      hobbies: new FormArray([], Validators.required)
+    });
+    this.addHobby()
+    this.userRegistrationForm.get('dateOfBirth').valueChanges.subscribe((value: Date) => {
+      if (value) {
+        const age = this.calculateAge(new Date(value));
+        this.userRegistrationForm.get('age').setValue(age);
+      }
+    });
+  }
+
+  //method for calculating age from DOB
+  calculateAge(birthday: Date): number {
+    const ageDifMs = Date.now() - birthday.getTime();
+    const ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  //method for adding a new hobby
+  addHobby() {
+    if (this.hobbies.length > 0) {
+      this.hobbies.push(new FormControl(''));
+    } else {
+      this.hobbies.push(new FormControl('',Validators.required));
+    }
+  }
+
+  //method for removing a hobby
+  removeHobby(index: number) {
+    this.hobbies.removeAt(index);
+  }
+
+  //method which will be called while clicking on save button and the values will be manipulated according to need
+  onSave() {
+    console.log(this.userRegistrationForm.value);
+    this._snackBar.open(`Hi ${this.userRegistrationForm.value.firstName}, data logged succesfully on the console`, 'Close');
+  }
+
+  //getter for the hobbies form array
+  get hobbies(): FormArray {
+    return this.userRegistrationForm.get('hobbies') as FormArray;
+  }
+
+}
+
